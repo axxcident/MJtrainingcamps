399e19eecd07fe66cd49e55635750ad8568df94a
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-07-15 15:00:17 +0200

    initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>hej</p>
+    <p>{{count}}</p>
+    <button @click="count++">Increment Count</button>
+    <p>{{ myData }}</p>
+    <p>{{ reversedData }}</p>
+    <button @click="myData = 'Updated Data'" >Update Data</button>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+const props = defineProps({
+  message: String,
+})
+const myData = ref('Initial Value')
+const reversedData = computed(() => myData.value.split('').reverse().join(''))
+const count = ref(0)
+
+</script>
+
+<style scoped>
+
+</style>
