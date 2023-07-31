c2402e499e59af319588b126b130d3a5bbaf6d33
Axel Olivecrona <olivecrona.axel@gmail.com>
2023-07-31 15:07:06 +0200

    börjat

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
-    <p>hej</p>
+  <div class="welcome-page" >
+    <h1 class="header-text" >MJ TRAINING CAMP</h1>
+    <div class="navigation-meny">
+      <a href="#">
+        <h4 class="meny-val">Om Oss</h4>
+      </a>
+      <a href="#">
+        <h4 class="meny-val">Träningsanläggningen</h4>
+      </a>
+      <a href="#">
+        <h4 class="meny-val">Phuket</h4>
+      </a>
+      <a href="#">
+        <h4 class="meny-val">Boka</h4>
+      </a>
+    </div>
+    <!-- <p>hej</p>
     <p>{{count}}</p>
     <button @click="count++">Increment Count</button>
     <p>{{ myData }}</p>
     <p>{{ reversedData }}</p>
     <button @click="myData = 'Updated Data'" >Update Data</button>
-    <p>{{ message }}</p>
+    <p>{{ message }}</p> -->
+    <img src="../assets/yellowPlank.svg" class="background-placeholder" alt="background placeholder" />
   </div>
 </template>
 
