b981af3d8d9ba985f12a1e8b3f39412bf96e88f6
Axel Olivecrona <olivecrona.axel@gmail.com>
2023-07-31 16:20:02 +0200

    gula plankan måste fixas

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -15,25 +15,27 @@
         <h4 class="meny-val">Boka</h4>
       </a>
     </div>
-    <!-- <p>hej</p>
-    <p>{{count}}</p>
-    <button @click="count++">Increment Count</button>
-    <p>{{ myData }}</p>
-    <p>{{ reversedData }}</p>
-    <button @click="myData = 'Updated Data'" >Update Data</button>
-    <p>{{ message }}</p> -->
-    <img src="../assets/yellowPlank.svg" class="background-placeholder" alt="background placeholder" />
+    <yellowPlank />
+    <!-- <img src="../assets/yellowPlank.svg" class="background-placeholder" alt="background placeholder" /> -->
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
-const props = defineProps({
-  message: String,
-})
-const myData = ref('Initial Value')
-const reversedData = computed(() => myData.value.split('').reverse().join(''))
-const count = ref(0)
+import yellowPlank from './yellowPlank.vue'
+    // <p>hej</p>
+    // <p>{{count}}</p>
+    // <button @click="count++">Increment Count</button>
+    // <p>{{ myData }}</p>
+    // <p>{{ reversedData }}</p>
+    // <button @click="myData = 'Updated Data'" >Update Data</button>
+    // <p>{{ message }}</p>
+// import { ref, computed } from 'vue'
+// const props = defineProps({
+//   message: String,
+// })
+// const myData = ref('Initial Value')
+// const reversedData = computed(() => myData.value.split('').reverse().join(''))
+// const count = ref(0)
 
 </script>
 
---
+++ src/components/yellowPlank.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="page-1">
+    <div class="welcome-info">
+      <div class="welcome-info-flex">
+        <h4 class="welcome-info-text">hej</h4>
+        <h4 class="welcome-info-text">hej</h4>
+        <h4 class="welcome-info-text">hej</h4>
+      </div>
+    </div>
+    <img src="../assets/yellowPlank.svg" class="background-placeholder" alt="background placeholder" />
+  </div>
+</template>
+
+<script setup>
+// Vue script code goes here
+</script>
+
+<style scoped>
+
+  .background-placeholder {
+    z-index: 0;
+  }
+
+  .welcome-info-flex {
+    display: flex;
+    justify-content: space-around;
+    width: 600px;
+  }
+  .welcome-info {
+    z-index: 1;
+    position: absolute;
+    bottom: 4em;
+    left: 20em;
+  }
+</style>
