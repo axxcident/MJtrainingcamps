62d1fbfe69badf560d518e01e2149d5b2bf1723a
Axel Olivecrona <olivecrona.axel@gmail.com>
2023-08-01 12:03:28 +0200

    firstPage done

--- src/components/yellowPlank.vue
+++ src/components/yellowPlank.vue
@@ -2,9 +2,9 @@
   <div class="page-1">
     <div class="welcome-info">
       <div class="welcome-info-flex">
-        <h4 class="welcome-info-text">hej</h4>
-        <h4 class="welcome-info-text">hej</h4>
-        <h4 class="welcome-info-text">hej</h4>
+        <h4 class="welcome-info-text">2 Träningspass<br>dagligen</h4>
+        <h4 class="welcome-info-text">Bo i hotel nära<br>stranden</h4>
+        <h4 class="welcome-info-text">Flyg tur/retur<br>1/12-11/12</h4>
       </div>
     </div>
     <img src="../assets/yellowPlank.svg" class="background-placeholder" alt="background placeholder" />
@@ -24,12 +24,20 @@
   .welcome-info-flex {
     display: flex;
     justify-content: space-around;
-    width: 600px;
+    /* width: 600px; */
   }
   .welcome-info {
     z-index: 1;
     position: absolute;
-    bottom: 4em;
-    left: 20em;
+    width: 80%;
+    margin-top: 5%;
+    margin-left: 5%;
+  }
+
+  .welcome-info-text {
+    color: #ffffff;
+    text-shadow: 2px 2px 3px black;
+    font-size: 1.6em;
+    font-weight: 600;
   }
 </style>
