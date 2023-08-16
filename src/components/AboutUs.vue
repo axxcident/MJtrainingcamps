bc1c580864fd4bd3e84d914e3e5e7cb41156f6aa
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-08-16 10:34:36 +0200

    about-page tillagt och fixat, behöver dock pages istallet for komponenter

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="about-us-page">
     <div class="about-square">
-      <h4>Vem är jag och vad<br>kan du förvänta dig?</h4>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, hic adipisci illo aperiam praesentium repudiandae officia saepe inventore obcaecati quo!</p>
+      <h4>Vem är jag och vad kan du förvänta dig?</h4>
+      <p>Hej jag heter Mathias Jonsson Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
     </div>
     <div class="matte-container">
-      <img src="matte.png" alt="Picture of coach">
+      <img src="matteb.png" alt="Picture of coach">
       <!-- <img :src="matteImage" alt="Picture of coach"> -->
     </div>
   </div>
@@ -21,4 +21,41 @@
   .about-us-page {
     position: relative;
   }
+
+  .matte-container {
+    position: fixed;
+    right: -7rem;
+    bottom: -40rem;
+  }
+
+  .about-square {
+    width: 437px;
+    height: 761px;
+    background-color: #FFD119;
+    border-radius: 67px;
+
+    position: fixed;
+    left: 13rem;
+    bottom: -14rem;
+
+    display: flex;
+    flex-direction: column;
+  }
+
+  .about-square h4, p {
+    font-family: 'Inika', serif;
+    width: 400px;
+    margin: 10px 19px;
+    padding: 2px 16px 0 16px;
+  }
+
+  .about-square h4 {
+    font-size: 36px;
+    font-weight: 500;
+  }
+
+  .about-square p {
+    font-size: 24px;
+  }
+
 </style>
